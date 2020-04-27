ea4a1ee23246fa693c722cc7feff8ac9738e9a92
milos <milos@screensailor.net>
2020-04-27 22:51:50 +0100

    import Peek

--- Package.swift
+++ Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics.git", .upToNextMajor(from: "0.0.5")),
+        .package(url: "https://github.com/screensailor/Peek.git", .branch("master")),
     ],
     targets: [
         .target(
             name: "Space",
             dependencies: [
                 .product(name: "Numerics", package: "swift-numerics"),
+                .byName(name: "Peek"),
             ]
         ),
     ]
