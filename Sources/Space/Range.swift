970331217b49b81a3350d358d5af9d34753859bb
milos <milos@screensailor.net>
2020-05-03 13:55:20 +0100

    add ClosedRange.advance(_:by:) & stride(over:by:)

--- Sources/Space/Range.swift
+++ Sources/Space/Range.swift
@@ -7,7 +7,24 @@ extension ClosedRange: Real2D where Bound: Real {
 }
 
 extension ClosedRange where Bound: AdditiveArithmetic {
+    
     @inlinable public var span: Bound { upperBound - lowerBound }
+    
+    public enum Advance: String, Codable, Equatable {
+        case upperBound
+        case lowerBound
+    }
+    
+    @inlinable public mutating func advance(_ advance: Advance, by offset: Bound) {
+        switch advance {
+        case .upperBound:
+            let o = upperBound + offset
+            self = Swift.min(o, lowerBound) ... o
+        case .lowerBound:
+            let o = lowerBound + offset
+            self = o ... Swift.max(o, upperBound)
+        }
+    }
 }
 
 extension ClosedRange {
@@ -25,4 +42,16 @@ extension ClosedRange {
     @inlinable public func clamped(to range: PartialRangeThrough<Bound>) -> Self {
         clamped(to: Swift.min(lowerBound, range.upperBound) ... range.upperBound)
     }
+}
+
+@inlinable public func stride<A>(over range: ClosedRange<A>, by stride: A.Stride) -> StrideThrough<A>
+    where A: Strideable
+{
+    Swift.stride(from: range.lowerBound, through: range.upperBound, by: stride)
+}
+
+@inlinable public func stride<A>(over range: Range<A>, by stride: A.Stride) -> StrideTo<A>
+    where A: Strideable
+{
+    Swift.stride(from: range.lowerBound, to: range.upperBound, by: stride)
 }
